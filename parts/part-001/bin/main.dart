6eb13d20739ed85789af7bae0dbbfdb393547206
Sanskar <sanskarin@outlook.in>
2026-08-18 19:46:19 +0530

    part-001: add runnable console example
    Signed-off-by: sanskarIN <sanskarin@outlook.in>

---
+++ parts/part-001/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:dfm_part_001/fundamentals.dart';
+
+void main() {
+  print(buildGreeting('Dart learner'));
+  print('25°C = ${celsiusToFahrenheit(25).toStringAsFixed(1)}°F');
+  print('42 is even: ${isEven(42)}');
+}
