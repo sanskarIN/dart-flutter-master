61073edb7343163e3b327d4daf3b1f777fb9fef6
Sanskar <sanskarin@outlook.in>
2026-08-18 19:54:39 +0530

    part-003: align executable with operators chapter
    Signed-off-by: sanskarIN <sanskarin@outlook.in>

--- parts/part-003/bin/main.dart
+++ parts/part-003/bin/main.dart
@@ -1,7 +1,8 @@
-import 'package:dfm_part_003/control_flow.dart';
+import 'package:dfm_part_003/operators.dart';
 
 void main() {
-  print('Score 88: ${classifyScore(88)}');
-  print('Even-number total: ${sumEvenNumbers([1, 2, 3, 4, 5, 6])}');
-  print(fizzBuzz(15).join(', '));
+  print('2 + 3 × 4 = ${evaluateWithPrecedence(2, 3, 4)}');
+  print('7 is within 1..10: ${isWithinInclusive(7, 1, 10)}');
+  print('Bitwise OR of 0b0011 and 0b0101: ${combineBitFlags(0x3, 0x5)}');
+  print('10 % 3 = ${safeRemainder(10, 3)}');
 }
