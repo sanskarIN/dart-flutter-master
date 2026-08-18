e4cb8d75def16105c229fd2af4e72efb54fd8504
Sanskar <sanskarin@outlook.in>
2026-08-18 19:48:13 +0530

    part-003: add runnable control-flow demo
    Signed-off-by: sanskarIN <sanskarin@outlook.in>

---
+++ parts/part-003/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:dfm_part_003/control_flow.dart';
+
+void main() {
+  print('Score 88: ${classifyScore(88)}');
+  print('Even-number total: ${sumEvenNumbers([1, 2, 3, 4, 5, 6])}');
+  print(fizzBuzz(15).join(', '));
+}
